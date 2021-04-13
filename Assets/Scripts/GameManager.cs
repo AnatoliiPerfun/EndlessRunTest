4737bd5cc9f760136d89c9a2612b014164bb4462
Anatolii Perfun <54056693+AnatoliiPerfun@users.noreply.github.com>
2021-04-13 21:11:15 +0300

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+
+public class GameManager : MonoBehaviour 
+{
+
+
+    [SerializeField] PlayerMovement playerMovement;
+    public static GameManager inst;
+
+
+    public void IncrementScore ()
+    {
+        playerMovement.speed += playerMovement.speedIncreasePerPoint;
+    }
+
+    private void Awake ()
+    {
+        inst = this;
+    }
+}
